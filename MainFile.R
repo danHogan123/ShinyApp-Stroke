a594d2bb09bb5ac87ea6d8c62a1dd03644012331
CarlosMedina-03 <cam.93337@gmail.com>
2024-06-07 10:50:05 -0400

    Setting up files

--- MainFile.R
+++ MainFile.R
@@ -1,4 +1,7 @@
 library(shiny)
+library(rio)
+
+strokeData <- import("healthcare-dataset-stroke-data.csv")
 
 ui <- fluidPage("This is course of statistical computing with R")
 server <- function(input, output) {}
