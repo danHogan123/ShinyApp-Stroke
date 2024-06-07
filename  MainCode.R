4969cf193a492c65770bcb07732620057182e1d4
CarlosMedina-03 <cam.93337@gmail.com>
2024-06-07 13:45:32 -0400

    Create  MainCode.R

---
+++  MainCode.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(rio)
+
+strokeData <- import("healthcare-dataset-stroke-data.csv")
+
+ui <- fluidPage("This is course of statistical computing with R")
+server <- function(input, output) {}
+shinyApp(ui = ui, server = server)
