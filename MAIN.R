6002fdc2d40798623a81ce84fbe6160635e983ae
ArthurAmes2 <arthurames@ufl.edu>
2024-06-14 11:53:04 -0400

    OKAGA

--- MAIN.R
+++ MAIN.R
@@ -4,3 +4,6 @@ library(readr)
 strokeDataSet <- read_csv("healthcare-dataset-stroke-data.csv")
 strokeDataSet
 
+
+model1 <- glm(stroke ~ age, data = strokeDataSet, family = binomial())
+model1
--- ShinyCode/SERVER.R
+++ ShinyCode/SERVER.R
@@ -19,5 +19,57 @@ shinyServer(function(input, output){
     }
     
   })
+<<<<<<< Updated upstream
+=======
+  output$dynamic_inputs <- renderUI({
+    predictor_inputs <- lapply(input$predictors, function(pred) {
+      if (pred == "Gender") {
+        selectInput(
+          inputId = "input_Gender",
+          label = "Gender:",
+          choices = c("Male", "Female"),
+          selected = "Male"
+        )
+      } else {
+        numericInput(
+          inputId = paste0("input_", pred),
+          label = pred,
+          value = 0
+        )
+      }
+    })
+    do.call(tagList, predictor_inputs)
+  })
+  
+  observeEvent(input$predict, {
+    req(input$predictors)
+    
+    # Prepare the data for logistic regression
+    predictors <- input$predictors[input$predictors != "Gender"]  # Exclude Gender from predictors for model building
+    formula <- as.formula(paste("Stroke ~", paste(predictors, collapse = " + ")))
+    model <- glm(formula, data = strokeDataSet, family = binomial)
+    
+    # Gather input values for prediction
+    new_data <- data.frame(matrix(ncol = length(predictors), nrow = 1))
+    colnames(new_data) <- predictors
+    for (pred in predictors) {
+      new_data[[pred]] <- input[[paste0("input_", pred)]]
+    }
+    
+    # Add Gender to the new_data for prediction
+    new_data$Gender <- input$input_Gender
+    
+    # Predict stroke probability
+    prediction <- predict(model, newdata = new_data, type = "response")
+    
+    # Display the prediction result
+    output$prediction_result <- renderText({
+      paste("Predicted probability of stroke:", round(prediction, 4))
+    })
+  })
+  
+  
+  
+>>>>>>> Stashed changes
   
 })
